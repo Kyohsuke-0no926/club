07d075cdec8fd1b5472cbb41c05395b91c6e1dfb
Kyohsuke-Ono926 <kyoh4k-gongyou@kuc.biglobe.ne.jp>
2021-10-12 21:32:39 +0900

    make_club

--- club.php
+++ club.php
@@ -18,35 +18,24 @@
     <h2>サークル</h2>
   </div>
   <?php
-    print("バスケットボール部");
-    $sql = "insert into club_name (id, name) values (3, \"バスケットボール部\")";
-    // $sql = "insert into club_name (name) values (\"バスケットボール部\")";
-    $result = $db->query($sql);
     $sql = "SELECT * FROM club_name";
     if ($result = $db->query($sql)) {
       $count = $result->num_rows;
-      $result->close(); // 結果セットを閉じる
+      $result->close(); 
     }
-    // $counts = $db->query('SELECT COUNT(*) as cnt FROM club_name');
-    // $count = $counts->fetch();
   ?>
   <div class="club-links">
   <?php
   $sql = "SELECT * FROM club_name";
   $club_names = [];
   if ($result = $db->query($sql)) {
-      // 連想配列を取得
       while ($row = $result->fetch_assoc()) {
         $club_names[]= $row;
       }
-      // 結果セットを閉じる
       $result->close();
   }
 
   for ($i=0; $i<$count; $i++):
-    // $club_names = $db->prepare('SELECT * FROM club_name WHERE id=?');
-    // $club_names->execute(array($i));
-    // $club_name = $club_names->fetch()
   ?>
   <div class="club-link">
   <p><a href="member.php?id=<?php print($club_names[$i]['id']); ?>"><?php print($club_names[$i]['name']); ?></a></p>
---
+++ make_club.php
@@ -0,0 +1,22 @@
+<?php
+  $sql = "insert into club_name (name) values (\"バスケットボール部\")";
+  $result = $db->query($sql);
+
+  $sql = "insert into club_name (name) values (\"ボランティアサークル\")";
+  $result = $db->query($sql);
+
+  $sql = "insert into club_name (name) values (\"弓道部\")";
+  $result = $db->query($sql);
+
+  $sql = "insert into club_name (name) values (\"フットサルサークル\")";
+  $result = $db->query($sql);
+
+  $sql = "insert into club_name (name) values (\"軽音サークル\")";
+  $result = $db->query($sql);
+
+  $sql = "insert into club_name (name) values (\"野球部\")";
+  $result = $db->query($sql);
+
+  $sql = "insert into club_name (name) values (\"テニスサークル\")";
+  $result = $db->query($sql);
+?>
