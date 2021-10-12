098ca92cab90f29457d83752fa5be5a9001ebc94
Kyohsuke-Ono926 <kyoh4k-gongyou@kuc.biglobe.ne.jp>
2021-10-12 20:34:25 +0900

    mysqli

--- dbconnect.php
+++ dbconnect.php
@@ -8,10 +8,10 @@
 //   echo 'DB接続エラー: ' . $e->getMEssage();
 // }
   $db = new mysqli('mysql:host=us-cdbr-east-04.cleardb.com;','bebd1ac9c20308', 'e289300a', 'heroku_4d0860ef942f6b0');
-  if ($mysqli->connect_error) {
-  echo $mysqli->connect_error;
+  if ($db->connect_error) {
+  echo $db->connect_error;
   exit();
 } else {
-  $mysqli->set_charset("utf8");
+  $db->set_charset("utf8");
 }
 ?>
