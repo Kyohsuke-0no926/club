8588c7c9978c0a05030bc39a24ed9b3b4cd0df2b
Kyohsuke-Ono926 <kyoh4k-gongyou@kuc.biglobe.ne.jp>
2021-10-12 21:49:05 +0900

    make_club

--- make_club.php
+++ make_club.php
@@ -1,23 +1,23 @@
 <?php require('dbconnect.php') ?>
 <?php
-  // $sql = "insert into club_name (name) values (\"バスケットボール部\")";
-  // $result = $db->query($sql);
+  $sql = "insert into club_name (id, name) values (1, \"バスケットボール部\")";
+  $result = $db->query($sql);
 
-  // $sql = "insert into club_name (name) values (\"ボランティアサークル\")";
-  // $result = $db->query($sql);
+  $sql = "insert into club_name (id, name) values (2, \"ボランティアサークル\")";
+  $result = $db->query($sql);
 
-  // $sql = "insert into club_name (name) values (\"弓道部\")";
-  // $result = $db->query($sql);
+  $sql = "insert into club_name (id, name) values (3, \"弓道部\")";
+  $result = $db->query($sql);
 
-  // $sql = "insert into club_name (name) values (\"フットサルサークル\")";
-  // $result = $db->query($sql);
+  $sql = "insert into club_name (id, name) values (4, \"フットサルサークル\")";
+  $result = $db->query($sql);
 
-  // $sql = "insert into club_name (name) values (\"軽音サークル\")";
-  // $result = $db->query($sql);
+  $sql = "insert into club_name (id, name) values (5, \"軽音サークル\")";
+  $result = $db->query($sql);
 
-  // $sql = "insert into club_name (name) values (\"野球部\")";
-  // $result = $db->query($sql);
+  $sql = "insert into club_name (id, name) values (6, \"野球部\")";
+  $result = $db->query($sql);
 
-  // $sql = "insert into club_name (name) values (\"テニスサークル\")";
-  // $result = $db->query($sql);
+  $sql = "insert into club_name (id, name) values (7, \"テニスサークル\")";
+  $result = $db->query($sql);
 ?>
