5afe75f0c02ed9c41bcfa7e6fc0d9b41eddb8d10
Kyohsuke-Ono926 <kyoh4k-gongyou@kuc.biglobe.ne.jp>
2021-10-12 20:33:26 +0900

    mysqli

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,17 @@
 <?php
-try {
+// try {
   // $db = new PDO('mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_4d0860ef942f6b0;charset=utf8','bebd1ac9c20308', 'e289300a');
-  $db = new mysqli('mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_4d0860ef942f6b0;charset=utf8','bebd1ac9c20308', 'e289300a');
-  $db->set_charset("utf8");
   // $db->query('SET character_set_client=utf8');
   // $db->query('SET character_set_results=utf8');
   // $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8','root', 'root');
-} catch (PDOException $e) {
-  echo 'DB接続エラー: ' . $e->getMEssage();
+// } catch (PDOException $e) {
+//   echo 'DB接続エラー: ' . $e->getMEssage();
+// }
+  $db = new mysqli('mysql:host=us-cdbr-east-04.cleardb.com;','bebd1ac9c20308', 'e289300a', 'heroku_4d0860ef942f6b0');
+  if ($mysqli->connect_error) {
+  echo $mysqli->connect_error;
+  exit();
+} else {
+  $mysqli->set_charset("utf8");
 }
 ?>
